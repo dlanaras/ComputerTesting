6b33e006ec511bd885600682f3d0c67126271e50
dlanaras <dimitrislanaras04@outlook.com>
2021-08-19 13:58:11 +0200

    pretty much finished except buggy Logarithms

--- ComputerSolution/Computer/Calculator.cs
+++ ComputerSolution/Computer/Calculator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace Computer
 {
@@ -48,9 +49,209 @@ namespace Computer
             }
         }
 
-        //TODO: Handle Exception with try and catch
-        //TODO: first check which operation then get numbers
 
+        public void UseCalculator()
+        {
+            this.TurnCalculatorOn();
+            string doneWithUsingCalc = "";
+            while (!doneWithUsingCalc.Equals("n"))
+            {
+                this.setUserCalcOps();
+
+                switch (this.CalcOps)
+                {
+                    case CalculatorOperations.Add:
+                        this.AdditionExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Subtract:
+                        this.SubtractionExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Multiply:
+                        this.MultiplicationExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Divide:
+                        this.DivisionExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Exponent:
+                        this.ExponentExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Root:
+                        this.RootExceptionHandling();
+                        break;
+
+                    case CalculatorOperations.Logarithm:
+                        this.LogarithmExceptionHandling();
+                        break;
+                    default:
+                        throw new Exception("CalcOps should be defined");
+                }
+
+                Console.WriteLine($"The Result is {this.Result}");
+                Console.WriteLine("Want to calculate anything else y/n?");
+                doneWithUsingCalc = Console.ReadLine();
+            }
+            this.TurnCalculatorOff();
+        }
+
+        private void ExponentExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("base", "exponent");
+                this.Result = CalculatorExponentiation.GetPowerOfBaseToExponent(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.ExponentExceptionHandling();
+            }
+        }
+
+        private void RootExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("degree", "radicand");
+                this.Result = CalculatorRoot.GetRootWithDegreeOfRadicand(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.RootExceptionHandling();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Degree cannot be 0, because dividing by 0 is not possible.\n");
+                this.RootExceptionHandling();
+            }
+        }
+
+        private void LogarithmExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("base", "radicand");
+                this.Result = CalculatorLogarithm.LogWithBaseAndRadicand(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.LogarithmExceptionHandling();
+            }
+
+        }
+
+        private void DivisionExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("dividen", "divisor");
+                this.Result = CalculatorDivision.Divide(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.DivisionExceptionHandling();
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.\n");
+                this.DivisionExceptionHandling();
+            }
+
+        }
+
+        private void MultiplicationExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("factor", "multiplicand");
+                this.Result = CalculatorMultiplication.Multiply(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.MultiplicationExceptionHandling();
+            }
+
+        }
+
+
+        private void SubtractionExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("minuend", "subtrahend");
+                this.Result = CalculatorSubtraction.Subtract(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.SubtractionExceptionHandling();
+            }
+
+        }
+
+        private void AdditionExceptionHandling()
+        {
+            try
+            {
+                this.setUserNums("summand", "addend");
+                this.Result = CalculatorAddition.Add(this.FirstNum, this.SecondNum);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The numbers you entered are too large or have to many decimal places\n");
+                this.AdditionExceptionHandling();
+            }
+
+        }
+
+        private void TurnCalculatorOff()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculator has been turned off. Press any key to close this window.");
+            Console.ReadKey();
+        }
+
+        private void TurnCalculatorOn()
+        {
+            Console.Write("Starting Calculator: \nProgress:|");
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("*");
+                Thread.Sleep(30);
+            }
+            Console.WriteLine("|\nCalculator has Started\n");
+        }
+
+        private void setUserNums(string firstNumMathName, string secondNumMathName)
+        {
+
+            string firstStringToConvertToDouble = this.getUserStringToConvertToDouble(firstNumMathName);
+            string secondStringToConvertToDouble = this.getUserStringToConvertToDouble(secondNumMathName);
+            try
+            {
+                this.FirstNum = CalculatorStringToDoubleConvertor.StringToDouble(firstStringToConvertToDouble);
+                this.SecondNum = CalculatorStringToDoubleConvertor.StringToDouble(secondStringToConvertToDouble);
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("That's not a valid number. Try again.\n");
+                this.setUserNums(firstNumMathName, secondNumMathName);
+            }
+        }
+
+        private string getUserStringToConvertToDouble(string mathematicalNumName)
+        {
+            Console.WriteLine($"Enter a {mathematicalNumName}: ");
+            return Console.ReadLine();
+        }
 
         private void setUserCalcOps()
         {
@@ -59,14 +260,13 @@ namespace Computer
             {
                 this.CalcOps = CalculatorInputToOperationsConvertor.ReturnUserInputAsOperation(userSuggestedOperation);
             }
-            catch(InvalidOperationException)
+            catch (InvalidOperationException)
             {
                 Console.WriteLine("That's not a valid operation\n");
                 this.setUserCalcOps();
             }
 
         }
-
 
         private string AskUserWhichOperation()
         {
---
+++ ComputerSolution/Computer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Computer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.UseCalculator();
+        }
+    }
+}
